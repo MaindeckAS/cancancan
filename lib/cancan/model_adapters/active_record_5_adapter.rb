7365c3e93b8bebc6ff24d10281ad4543e4157a81
Martin Ulleberg <martin.ulleberg@gmail.com>
2020-11-23 03:01:02 +0100

    Remove call to resolve_column_aliases

--- lib/cancan/model_adapters/active_record_5_adapter.rb
+++ lib/cancan/model_adapters/active_record_5_adapter.rb
@@ -6,7 +6,7 @@ module CanCan
       AbstractAdapter.inherited(self)
 
       def self.for_class?(model_class)
-        version_greater_or_equal?('5.0.0') && model_class <= ActiveRecord::Base
+        version_greater_or_equal?("5.0.0") && model_class <= ActiveRecord::Base
       end
 
       # rails 5 is capable of using strings in enum
@@ -23,16 +23,15 @@ module CanCan
 
       def build_relation(*where_conditions)
         if joins.present?
-          inner = @model_class.unscoped do
+          inner = @model_class.unscoped {
             @model_class.left_joins(joins).where(*where_conditions)
-          end
+          }
           @model_class.where(@model_class.primary_key => inner)
         else
           @model_class.where(*where_conditions)
         end
       end
 
-      # Rails 4.2 deprecates `sanitize_sql_hash_for_conditions`
       def sanitize_sql(conditions)
         if conditions.is_a?(Hash)
           sanitize_sql_activerecord5(conditions)
@@ -46,16 +45,12 @@ module CanCan
         table_metadata = ActiveRecord::TableMetadata.new(@model_class, table)
         predicate_builder = ActiveRecord::PredicateBuilder.new(table_metadata)
 
-        conditions = predicate_builder.resolve_column_aliases(conditions)
-
-        conditions.stringify_keys!
-
-        predicate_builder.build_from_hash(conditions).map { |b| visit_nodes(b) }.join(' AND ')
+        predicate_builder.build_from_hash(conditions.stringify_keys).map { |b| visit_nodes(b) }.join(" AND ")
       end
 
       def visit_nodes(node)
         # Rails 5.2 adds a BindParam node that prevents the visitor method from properly compiling the SQL query
-        if self.class.version_greater_or_equal?('5.2.0')
+        if self.class.version_greater_or_equal?("5.2.0")
           connection = @model_class.send(:connection)
           collector = Arel::Collectors::SubstituteBinds.new(connection, Arel::Collectors::SQLString.new)
           connection.visitor.accept(node, collector).value
